317c725b31ee0684cb7afb6e5a488a3287dd63df
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2026-01-03 21:55:11 +0100

    Initialize lvgl data buffer statically instead of dynamically.
    This allows double buffering and is safer as the memory is reserved at compile time instead of runtime.

--- Core/Inc/display.h
+++ Core/Inc/display.h
@@ -3,13 +3,6 @@
 
 #include "lvgl.h"
 
-#define LCD_H_RES             320
-#define LCD_V_RES             480
-#define BUS_SPI1_POLL_TIMEOUT 0x1000U
-
-#define BYTES_PER_PIXEL       (LV_COLOR_FORMAT_GET_SIZE(LV_COLOR_FORMAT_RGB565))
-#define BUFF_SIZE             (LCD_V_RES * 10 * BYTES_PER_PIXEL)
-
 void display_initLvgl(void);
 
 #endif /* DISPLAY_H */
