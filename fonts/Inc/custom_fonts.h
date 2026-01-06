7162d4a9a883036ce2df5332a0f839db03da1329
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2026-01-06 17:17:43 +0100

    Add custom size 22 font

--- fonts/Inc/custom_fonts.h
+++ fonts/Inc/custom_fonts.h
@@ -7,5 +7,6 @@
 LV_FONT_DECLARE(custom_font_montserrat_44);
 LV_FONT_DECLARE(custom_font_montserrat_34);
 LV_FONT_DECLARE(custom_font_montserrat_18);
+LV_FONT_DECLARE(custom_font_montserrat_22);
 
 #endif /* CUSTOM_FONTS_H */
