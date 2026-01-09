081a6ec7be86219e1c68afb686cca4023c93ef8b
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2026-01-09 20:23:59 +0100

    Enable i2c interrupt mode

--- Core/Inc/stm32wbxx_it.h
+++ Core/Inc/stm32wbxx_it.h
@@ -57,6 +57,8 @@ void EXTI1_IRQHandler(void);
 void EXTI4_IRQHandler(void);
 void DMA1_Channel1_IRQHandler(void);
 void TIM2_IRQHandler(void);
+void I2C1_EV_IRQHandler(void);
+void I2C1_ER_IRQHandler(void);
 void SPI1_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
