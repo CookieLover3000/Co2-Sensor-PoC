42186c88d837e5e53ba999079068d3add467908e
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2026-01-11 14:34:38 +0100

    Add TIM16 PWM CubeMX configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -51,6 +51,7 @@ SPI_HandleTypeDef hspi1;
 DMA_HandleTypeDef hdma_spi1_tx;
 
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim16;
 
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
@@ -68,6 +69,7 @@ static void MX_DMA_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_TIM16_Init(void);
 static void MX_RF_Init(void);
 void StartDefaultTask(void *argument);
 
@@ -116,9 +118,10 @@ int main(void)
     MX_I2C1_Init();
     MX_SPI1_Init();
     MX_TIM1_Init();
+    MX_TIM16_Init();
     MX_RF_Init();
     /* USER CODE BEGIN 2 */
-
+    
     /* USER CODE END 2 */
 
     /* Init scheduler */
@@ -404,6 +407,64 @@ static void MX_TIM1_Init(void)
 }
 
 /**
+ * @brief TIM16 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_TIM16_Init(void)
+{
+
+    /* USER CODE BEGIN TIM16_Init 0 */
+
+    /* USER CODE END TIM16_Init 0 */
+
+    TIM_OC_InitTypeDef sConfigOC = {0};
+    TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+    /* USER CODE BEGIN TIM16_Init 1 */
+
+    /* USER CODE END TIM16_Init 1 */
+    htim16.Instance = TIM16;
+    htim16.Init.Prescaler = 31;
+    htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim16.Init.Period = 999;
+    htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim16.Init.RepetitionCounter = 0;
+    htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+    if (HAL_TIM_Base_Init(&htim16) != HAL_OK) {
+        Error_Handler();
+    }
+    if (HAL_TIM_PWM_Init(&htim16) != HAL_OK) {
+        Error_Handler();
+    }
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = 0;
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+    sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+    sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+    if (HAL_TIM_PWM_ConfigChannel(&htim16, &sConfigOC, TIM_CHANNEL_1) != HAL_OK) {
+        Error_Handler();
+    }
+    sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+    sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+    sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+    sBreakDeadTimeConfig.DeadTime = 0;
+    sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+    sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+    sBreakDeadTimeConfig.BreakFilter = 0;
+    sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+    if (HAL_TIMEx_ConfigBreakDeadTime(&htim16, &sBreakDeadTimeConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN TIM16_Init 2 */
+
+    /* USER CODE END TIM16_Init 2 */
+    HAL_TIM_MspPostInit(&htim16);
+}
+
+/**
  * Enable DMA controller clock
  */
 static void MX_DMA_Init(void)
@@ -443,7 +504,7 @@ static void MX_GPIO_Init(void)
     HAL_GPIO_WritePin(GPIOC, LCD_DC_Pin | LCD_RST_Pin, GPIO_PIN_SET);
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(LCD_BL_GPIO_Port, LCD_BL_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(LCD_BL_OLD_GPIO_Port, LCD_BL_OLD_Pin, GPIO_PIN_RESET);
 
     /*Configure GPIO pins : LCD_CS_Pin LCD_DC_Pin */
     GPIO_InitStruct.Pin = LCD_CS_Pin | LCD_DC_Pin;
@@ -459,12 +520,12 @@ static void MX_GPIO_Init(void)
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(LCD_RST_GPIO_Port, &GPIO_InitStruct);
 
-    /*Configure GPIO pin : LCD_BL_Pin */
-    GPIO_InitStruct.Pin = LCD_BL_Pin;
+    /*Configure GPIO pin : LCD_BL_OLD_Pin */
+    GPIO_InitStruct.Pin = LCD_BL_OLD_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    HAL_GPIO_Init(LCD_BL_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(LCD_BL_OLD_GPIO_Port, &GPIO_InitStruct);
 
     /*Configure GPIO pins : USB_DM_Pin USB_DP_Pin */
     GPIO_InitStruct.Pin = USB_DM_Pin | USB_DP_Pin;
--- Core/Src/stm32wbxx_hal_msp.c
+++ Core/Src/stm32wbxx_hal_msp.c
@@ -58,6 +58,8 @@ extern DMA_HandleTypeDef hdma_spi1_tx;
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
 /**
  * Initializes the Global MSP.
  */
@@ -267,9 +269,42 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
         /* USER CODE BEGIN TIM1_MspInit 1 */
 
         /* USER CODE END TIM1_MspInit 1 */
+    } else if (htim_base->Instance == TIM16) {
+        /* USER CODE BEGIN TIM16_MspInit 0 */
+
+        /* USER CODE END TIM16_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_TIM16_CLK_ENABLE();
+        /* USER CODE BEGIN TIM16_MspInit 1 */
+
+        /* USER CODE END TIM16_MspInit 1 */
     }
 }
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if (htim->Instance == TIM16) {
+        /* USER CODE BEGIN TIM16_MspPostInit 0 */
+
+        /* USER CODE END TIM16_MspPostInit 0 */
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**TIM16 GPIO Configuration
+        PA6     ------> TIM16_CH1
+        */
+        GPIO_InitStruct.Pin = LCD_BL_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Alternate = GPIO_AF14_TIM16;
+        HAL_GPIO_Init(LCD_BL_GPIO_Port, &GPIO_InitStruct);
+
+        /* USER CODE BEGIN TIM16_MspPostInit 1 */
+
+        /* USER CODE END TIM16_MspPostInit 1 */
+    }
+}
 /**
  * @brief TIM_Base MSP De-Initialization
  * This function freeze the hardware resources used in this example
@@ -287,6 +322,15 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim_base)
         /* USER CODE BEGIN TIM1_MspDeInit 1 */
 
         /* USER CODE END TIM1_MspDeInit 1 */
+    } else if (htim_base->Instance == TIM16) {
+        /* USER CODE BEGIN TIM16_MspDeInit 0 */
+
+        /* USER CODE END TIM16_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM16_CLK_DISABLE();
+        /* USER CODE BEGIN TIM16_MspDeInit 1 */
+
+        /* USER CODE END TIM16_MspDeInit 1 */
     }
 }
 
